658a04a789f15ec5f4e2712cc3f2926d0a5f5b27
sesraaksu <sesraaksu@outlook.com>
2023-06-29 20:34:40 +0300

    Taksimetre Pratiği

---
+++ Taksimetre.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Taksimetre {
+    public static void main(String[] args) {
+        int km;
+        double perkm = 2.20; // km başına
+        double total;
+        double startPrize = 10;
+
+        Scanner imput = new Scanner(System.in);
+        System.out.print("mesafiyi km cinsinden giriniz : ");
+        km = imput.nextInt();
+
+        total = km * perkm;
+        total += startPrize;
+
+        total = ( total < 20) ? 20 : total;
+        System.out.print("toplam tutarız : " + total);
+
+    }
+}
