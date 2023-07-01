dd376c4901dd5e4c6c2aaded44ad9ac3afde876d
sesraaksu <sesraaksu@outlook.com>
2023-07-01 13:23:32 +0300

    artık yıl hesaplama

---
+++ ArtikYil.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ArtikYil {
+    public static void main(String[] args) {
+
+        int yil;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl giriniz : ");
+        yil = input.nextInt();
+
+        if (yil % 4 == 0 && yil % 100 != 0 ||  yil % 400 == 0 ){
+            System.out.print(yil + " bir artık yıldır");
+        } else {
+            System.out.print(yil + " bir artık yıl değildir");
+        }
+    }
+}
