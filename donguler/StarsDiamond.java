7148264b4b421f9709fe3b957f85d917067fc5c2
sesraaksu <sesraaksu@outlook.com>
2023-07-02 15:18:01 +0300

    StarsDiamond

---
+++ donguler/StarsDiamond.java
@@ -0,0 +1,36 @@
+package donguler;
+import java.util.Scanner;
+
+public class StarsDiamond {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+
+            for (int k = 1; k <= (n -i); k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= ((2 * i) - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+        for(int i=n;i>=0;i--){
+
+            for(int j=1;j <=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int k=1;k<=(2*i)-1;k++){
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+    }
+}
