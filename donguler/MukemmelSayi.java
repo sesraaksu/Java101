2a3b7c76a2a63d78f0e06435f4e3489d0150c58d
sesraaksu <sesraaksu@outlook.com>
2023-07-03 00:02:38 +0300

    mükemmel sayı

---
+++ donguler/MukemmelSayi.java
@@ -0,0 +1,26 @@
+package donguler;
+import java.util.Scanner;
+
+public class MukemmelSayi {
+
+    public static void main(String[] args) {
+
+        int toplam = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        int sayi = scanner.nextInt();
+
+        for(int i = 1; i < sayi; i++) {
+            if(sayi % i == 0) {
+                toplam += i;
+            }
+        }
+
+        if(sayi == toplam) {
+            System.out.println(sayi + " mükemmel bir sayıdır");
+        } else {
+            System.out.println(sayi + " mükemmel bir sayı degildir");
+        }
+    }
+}
