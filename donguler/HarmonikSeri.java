ff3b796ce813d962539a3dd97e55fa63139aeed0
sesraaksu <sesraaksu@outlook.com>
2023-07-02 14:44:56 +0300

    Harmonik Seri

---
+++ donguler/HarmonikSeri.java
@@ -0,0 +1,18 @@
+package donguler;
+import java.util.Scanner;
+
+public class HarmonikSeri {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n sayısını giriniz : ");
+        int n = scanner.nextInt();
+
+        double result = 0;
+
+        for (double i = 1; i <= n; i++){
+            result += (1/i);
+        }
+        System.out.print("sonuç : " + result);
+    }
+}
