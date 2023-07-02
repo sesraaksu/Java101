97c4fbbea067dc21a0134c3a9c0c2998e79155ac
sesraaksu <sesraaksu@outlook.com>
2023-07-03 00:06:47 +0300

    ters üçgen

--- donguler/TersUcgen.java
+++ donguler/TersUcgen.java
@@ -1,25 +1,12 @@
 package donguler;
 import java.util.Scanner;
 
-public class StarsDiamond {
+public class TersUcgen {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         System.out.print("Bir Sayı Giriniz :");
         int n = input.nextInt();
-
-        for (int i = 1; i <= n ; i++) {
-
-            for (int k = 1; k <= (n -i); k++) {
-                System.out.print(" ");
-            }
-
-            for (int j = 1; j <= ((2 * i) - 1); j++) {
-                System.out.print("*");
-            }
-
-            System.out.println(" ");
-        }
 
         for(int i=n;i>=0;i--){
 
